773af349c421c35eccf70b1c2e8450470cd3a567
John Kilima <jkbundala@gmail.com>
2024-02-09 14:35:24 +0530

    Project - JPA Example : Get Student operation

--- jpa-example/src/main/java/dev/kilima/jpaexample/GetData.java
+++ jpa-example/src/main/java/dev/kilima/jpaexample/GetData.java
@@ -1,9 +1,12 @@
 package dev.kilima.jpaexample;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import dev.kilima.jpaexample.beans.Student;
 
@@ -14,10 +17,17 @@ public class GetData {
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("kilima_dev_company");
 		EntityManager manager = factory.createEntityManager();
 		EntityTransaction tran = manager.getTransaction();
-		
+
 		// to get single object
 		Student std = manager.find(Student.class, 3);
-		System.out.println(std.getRegno()+" "+std.getStdname()+" "+std.getDob());
+		System.out.println(std.getRegno() + " " + std.getStdname() + " " + std.getDob());
+
+		// to get all students objects
+		System.out.println("** to get all students records from database **");
+		Query qry = manager.createQuery("from Student");
+		List<Student> stdlist = qry.getResultList();
+		for (Student s : stdlist)
+			System.out.println(s.getRegno() + " " + s.getStdname() + " " + s.getDob());
 
 	}
 
