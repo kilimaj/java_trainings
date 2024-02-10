9e6bd05a7615e29c4eba680e8ff93368ae911073
John Kilima <jkbundala@gmail.com>
2024-02-10 10:43:41 +0530

    Project - SpringBoot with JPA : Create the BooksMain class

---
+++ springboot-jpa/src/main/java/dev/kilima/library/BooksMain.java
@@ -0,0 +1,14 @@
+package dev.kilima.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksMain.class, args);
+
+	}
+
+}
