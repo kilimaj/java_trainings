b025322ee2e54909c34e6d1c2327f8556869b17e
John Kilima <jkbundala@gmail.com>
2024-02-13 10:28:12 +0530

    Project - SpringBoot Mybatis Example : Employee - Create Employee bean class

---
+++ springboot-mybatis/src/main/java/dev/kilima/springmybatis/empl/beans/Employee.java
@@ -0,0 +1,42 @@
+package dev.kilima.springmybatis.empl.beans;
+
+public class Employee {
+
+	private int empid;
+	private String empname;
+	private double salary;
+	private String doj;
+
+	public int getEmpid() {
+		return empid;
+	}
+
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+
+	public String getEmpname() {
+		return empname;
+	}
+
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public String getDoj() {
+		return doj;
+	}
+
+	public void setDoj(String doj) {
+		this.doj = doj;
+	}
+
+}
