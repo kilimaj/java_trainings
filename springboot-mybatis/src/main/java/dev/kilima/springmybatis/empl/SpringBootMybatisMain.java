d2126f5e762a562ea1fa145b16709c318ec813db
John Kilima <jkbundala@gmail.com>
2024-02-13 10:07:29 +0530

    Project - SpringBoot Mybatis Example : Employee - Creating the main class

---
+++ springboot-mybatis/src/main/java/dev/kilima/springmybatis/empl/SpringBootMybatisMain.java
@@ -0,0 +1,14 @@
+package dev.kilima.springmybatis.empl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybatisMain.class, args);
+
+	}
+
+}
