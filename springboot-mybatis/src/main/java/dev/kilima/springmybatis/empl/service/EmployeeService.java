59c6ce63dc9951d383c411a46406afc2ded6bdb7
John Kilima <jkbundala@gmail.com>
2024-02-13 11:59:40 +0530

    Project - SpringBoot Mybatis Example : Employee - Service for all other CRUD operations

--- springboot-mybatis/src/main/java/dev/kilima/springmybatis/empl/service/EmployeeService.java
+++ springboot-mybatis/src/main/java/dev/kilima/springmybatis/empl/service/EmployeeService.java
@@ -1,8 +1,18 @@
 package dev.kilima.springmybatis.empl.service;
 
+import java.util.List;
+
 import dev.kilima.springmybatis.empl.beans.Employee;
 
 public interface EmployeeService {
 
 	int addEmployee(Employee emp);
+	
+	int updateEmployee(Employee emp);
+	
+	int deleteEmployee(int empid);
+	
+	List<Employee> getAllEmployee();
+	
+	Employee getById(int empid);
 }
--- springboot-mybatis/src/main/java/dev/kilima/springmybatis/empl/service/impl/EmployeeServiceImpl.java
+++ springboot-mybatis/src/main/java/dev/kilima/springmybatis/empl/service/impl/EmployeeServiceImpl.java
@@ -1,5 +1,7 @@
 package dev.kilima.springmybatis.empl.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,8 +10,8 @@ import dev.kilima.springmybatis.empl.dao.EmployeeDao;
 import dev.kilima.springmybatis.empl.service.EmployeeService;
 
 @Service
-public class EmployeeServiceImpl implements EmployeeService{
-	
+public class EmployeeServiceImpl implements EmployeeService {
+
 	@Autowired
 	EmployeeDao dao;
 
@@ -18,5 +20,29 @@ public class EmployeeServiceImpl implements EmployeeService{
 		// TODO Auto-generated method stub
 		return dao.addEmployee(emp);
 	}
-	
+
+	@Override
+	public int updateEmployee(Employee emp) {
+		// TODO Auto-generated method stub
+		return dao.updateEmployee(emp);
+	}
+
+	@Override
+	public int deleteEmployee(int empid) {
+		// TODO Auto-generated method stub
+		return dao.deleteEmployee(empid);
+	}
+
+	@Override
+	public List<Employee> getAllEmployee() {
+		// TODO Auto-generated method stub
+		return dao.getAllEmployee();
+	}
+
+	@Override
+	public Employee getById(int empid) {
+		// TODO Auto-generated method stub
+		return dao.getById(empid);
+	}
+
 }
