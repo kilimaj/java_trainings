d99b1d7a4163d803a88a39253b7f0f76046ae494
John Kilima <jkbundala@gmail.com>
2024-02-13 10:31:39 +0530

    Project - SpringBoot Mybatis Example : Employee - Create Employee Dao interface

---
+++ springboot-mybatis/src/main/java/dev/kilima/springmybatis/empl/dao/EmployeeDao.java
@@ -0,0 +1,13 @@
+package dev.kilima.springmybatis.empl.dao;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+import dev.kilima.springmybatis.empl.beans.Employee;
+
+@Mapper
+public interface EmployeeDao {
+
+	@Insert("insert into employee values (#{empid},#{empname}, #{salary}, #{doj}")
+	int addEmployee(Employee emp);
+}
