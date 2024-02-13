ac514c0146c86095d91b72d175193177a5cb56f9
John Kilima <jkbundala@gmail.com>
2024-02-13 11:57:03 +0530

    Project - SpringBoot Mybatis Example : Employee - Employee dao queries for all other operations

--- springboot-mybatis/src/main/java/dev/kilima/springmybatis/empl/dao/EmployeeDao.java
+++ springboot-mybatis/src/main/java/dev/kilima/springmybatis/empl/dao/EmployeeDao.java
@@ -1,7 +1,12 @@
 package dev.kilima.springmybatis.empl.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import dev.kilima.springmybatis.empl.beans.Employee;
 
@@ -10,4 +15,17 @@ public interface EmployeeDao {
 
 	@Insert("insert into employee values (#{empid},#{empname}, #{salary}, #{doj})")
 	int addEmployee(Employee emp);
+
+	@Update("update employee set empname=#{empname}, salary=#{salary}, doj=#{doj} where empid=#{empid}")
+	int updateEmployee(Employee emp);
+
+	@Delete("delete from employee where empid=#{empid}")
+	int deleteEmployee(int empid);
+
+	@Select("select * from employee")
+	List<Employee> getAllEmployee();
+
+	@Select("select * from employee where empid=#{empid}")
+	Employee getById(int empid);
+
 }
